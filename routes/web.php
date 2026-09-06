17514e652b07f997fc91be73ab399b78ae599745
fresarema <franciscoesteban.arevalo@gmail.com>
2026-09-06 18:28:19 -0300

    correccion en ruta de componente Monitoreo en tiempo real

--- routes/web.php
+++ routes/web.php
@@ -47,8 +47,10 @@ Route::middleware(['auth'])->group(function () {
         Route::get('/usuarios/{id}/editar', [UsuarioController::class, 'edit'])->name('usuarios.edit');
         Route::put('/usuarios/{id}', [UsuarioController::class, 'update'])->name('usuarios.update');
         Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy'])->name('usuarios.destroy');
-        Route::get('/monitoreo', function () {return view('monitoreo.index'); })->name('monitoreo.index');
+        
     });
+
+    Route::get('/monitoreo', function () {return view('monitoreo.index'); })->name('monitoreo.index');
 
     // Rutas del Módulo de Inventarios
     Route::get('/inventarios', [InventarioController::class, 'index'])->name('inventarios.index');
